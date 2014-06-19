3953bfecb2eacec29fdd6e6f0395868375a1e1ea
Matthew Forr <matthew.forr@gmail.com>
2014-06-19 17:45:15 -0400

    Added Kadira and revised update function

--- sitemapper-meteor/lib/methods.js
+++ sitemapper-meteor/lib/methods.js
@@ -18,7 +18,12 @@ Meteor.methods({
 
   updateSiteStatus: function (site) {
     var data = _.omit(site, ['host', '_id', 'created_at']);
-    SiteScans.update({ _id: site._id }, { $set: data });
+    SiteScans.update({ _id: site._id }, { $set: data }, function (err, result) {
+      if (err) {
+        console.log(err);
+        return;  
+      }
+    });
     // SiteScans.update({ _id: site._id }, { $set: { status: site.status, pagesLeft: site.pagesLeft, pagesScanned: site.pagesScanned } });
     return site.host + ' ' + site._id + ' with status ' + site.status;
   }
---
+++ sitemapper-meteor/server/kadira.js
@@ -0,0 +1,1 @@
+Kadira.connect('CuwBuzCWfywWQ4hMj', 'ea7568a3-61a0-484a-9518-c2872d89c47a')
