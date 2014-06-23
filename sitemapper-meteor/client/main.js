3ae0b0437d91b3364f65607a4239b2307c7eac8b
Matthew Forr <matthew.forr@gmail.com>
2014-06-23 12:03:31 -0400

    Create page now should redirect on creation

--- sitemapper-meteor/client/main.js
+++ sitemapper-meteor/client/main.js
@@ -9,10 +9,11 @@ Template.create.events({
     var newStatus = tmp.find('input').value;
     var now = new Date().getTime();
 
-    // Check if a URL or better yet, delegate to a dedicated func
+    // TODO - Check if a URL or better yet, delegate to a dedicated func
     if (newStatus !== '' || newStatus !== undefined) {
       SiteScans.insert({ host: newStatus, created_at: now, pagesLeft: 0, pagesScanned: 0, status: 0 });
     }
+    Router.go('home');
   }
 });
 
